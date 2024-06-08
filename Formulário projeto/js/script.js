f4c1caa6f38d39d2868f278f5fd503fcdb62040e
“artur-pedro” <pedro.costa@lsdi.ufma.br>
2024-06-08 14:54:24 -0300

    add

--- Formulário projeto/js/script.js
+++ Formulário projeto/js/script.js
@@ -1,26 +1,48 @@
 
-const dnbRadio = document.getElementById('dnb');
-const nonDnbRadio = document.getElementById('nondnb');
 const optionalBox = document.querySelector('.optional-box');
 optionalBox.style.display = 'none';
 const checkboxes = optionalBox.querySelectorAll('input[type="checkbox"]');
-checkboxes.forEach(checkbox => {
-    checkbox.disabled = true
-});
-function toggleOptionalBox() {
-    if (dnbRadio.checked) {
-        optionalBox.style.display = 'block';
+
+async function showSecond(target) {
+    await fadeOut(optionalbox);
+    await fadeIn(optionalbox);
+}
+function fadeOut(target, duration = 500) {
+    return new Promise(resolve =>{
+        const animationEnded = () => {
+            target.style.display = 'none';
+            target.onanimationend = null;
+            target.style.animation = null;
+            resolve();
+        };
+        target.onanimationend = animationEnded;
+        target.style.animation = `fade-out ${duration}ms 1`;
+        checkboxes.forEach(checkbox => {
+            checkbox.disabled = true
+            checkbox.checked = false;
+        });
+    })
+}
+function fadeIn(target, duration = 500, display = 'block') {
+    return new Promise(resolve =>{
+        const animationEnded = () => {
+            target.onanimationend = null;
+            target.style.animation = null;
+            resolve();
+        };
+        target.onanimationend = animationEnded;
+        target.style.display = display;
+        target.style.animation = `fade-in ${duration}ms 1`;
         checkboxes.forEach(checkbox => {
             checkbox.disabled = false
-            checkbox.checked = false;
-        });
+        })
+    })
+}
+function handleRadioClick(isDnb) {
+    const optionalBox = document.getElementById('optional-box');
+    if (isDnb) {
+        fadeIn(optionalBox);
     } else {
-        optionalBox.style.display = 'none';
-        checkboxes.forEach(checkbox => {
-            checkbox.disabled = true;
-            checkbox.checked = false;
-        });
+        fadeOut(optionalBox);
     }
 }
-dnbRadio.addEventListener('change', toggleOptionalBox);
-nonDnbRadio.addEventListener('change', toggleOptionalBox);
